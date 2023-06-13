e0212101149cabdfa3d0c7a0f8763b5d89fd816e
Caua Santanna <emostalook@gmail.com>
2023-06-13 12:31:11 -0300

    eu dei meu cu

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -1,15 +1,17 @@
 package com.itb.inf2fm.comercio.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.itb.inf2fm.comercio.model.Produto;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
 	
-	
 	@GetMapping("/listar")
 	public String listarProdutos() {
+		
+		Produto p1 = new Produto();
+		p1.id = 20l;
 		
 		return "produtos.html"; 
 		
--- src/main/java/com/itb/inf2fm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -13,4 +13,47 @@ public class Produto {
 	//		   da propria classe.
 	//		   Entende-se por membros (atributos e ou métodos)
 
+	//Método Setter's e Getter's: atribuir e recuperar dados do atributo respectivamente
+	
+		public void setId(Long id) {
+			this.id = id;
+		}
+		
+		public Long getId() {
+			return id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public String getCodigobarras() {
+			return codigobarras;
+		}
+
+		public void setCodigobarras(String codigobarras) {
+			this.codigobarras = codigobarras;
+		}
+
+		public Double getPreco() {
+			return preco;
+		}
+
+		public void setPreco(Double preco) {
+			this.preco = preco;
+		}
+		
+		
 }
