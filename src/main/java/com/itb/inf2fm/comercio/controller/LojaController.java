42c9c3eeb897c4a3a9453e860eb844262fd7b14f
Caua Santanna <emostalook@gmail.com>
2023-06-26 08:29:50 -0300

    aaaaaa

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -23,23 +23,24 @@ public class LojaController {
 		p1.setPreco(3215.89);
 		
 		Produto p2 = new Produto();
-		p1.setId(20l);
-		p1.setNome("Fogão 4 bocas eletrolux");
-		p1.setCodigobarras("411M3UC00KK");
-		p1.setDescricao("esquenta mais que a boate kiss");
-		p1.setPreco(3999.99);
+		p2.setId(20l);
+		p2.setNome("Fogão 4 bocas eletrolux");
+		p2.setCodigobarras("411M3UC00KK");
+		p2.setDescricao("esquenta mais que a boate kiss");
+		p2.setPreco(3999.99);
 		
 		Produto p3 = new Produto();
-		p1.setId(20l);
-		p1.setNome("Computador Pichau Gmaer LED RGB ULTRA PRO kk");
-		p1.setCodigobarras("2500HOMENSDIFERENTES");
-		p1.setDescricao("roda pou a 15fps");
-		p1.setPreco(5999.99);
+		p3.setId(20l);
+		p3.setNome("Computador Pichau Gmaer LED RGB ULTRA PRO kk");
+		p3.setCodigobarras("2500HOMENSDIFERENTES");
+		p3.setDescricao("roda pou a 15fps");
+		p3.setPreco(5999.99);
 		
 		//add os produtos a lista
 		
 		listaDeProdutos.add(p1);
 		listaDeProdutos.add(p2);
+		listaDeProdutos.add(p3);
 		
 		model.addAttribute("listaDeProdutos",listaDeProdutos);
 		
